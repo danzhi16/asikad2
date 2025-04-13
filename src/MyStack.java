ea4235c0900d2ab8c5696100bb6e6a346a0ffb7f
Boranbayev Danial <1.dani.qwerty@gmail.com>
2025-04-13 20:33:55 +0500

    Implement MyStack class with push, pop, peek, and isEmpty methods

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,4 +1,26 @@
 package src;
 
-public class MyStack {
+/**
+ * Stack implementation using MyArrayList (LIFO behavior).
+ */
+public class MyStack<T> {
+    private final MyList<T> list = new MyArrayList<>();
+
+    public void push(T item) {
+        list.addLast(item);
+    }
+
+    public T pop() {
+        T last = list.getLast();
+        list.removeLast();
+        return last;
+    }
+
+    public T peek() {
+        return list.getLast();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
 }
